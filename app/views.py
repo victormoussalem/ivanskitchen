8078cd55f8d37c43a041f6124da94f26ecb41cbe
Victor <victor.moussalem@gmail.com>
2016-01-19 00:34:39 -0800

    changes to template

--- app/views.py
+++ app/views.py
@@ -4,8 +4,8 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
-
-@app.route('/NOM')
-def indexNOM():
     return render_template('indexNOM.html')
+
+@app.route('/test')
+def indexNOM():
+    return render_template('index.html')
